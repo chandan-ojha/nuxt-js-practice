12d1c7b5d134126236f63e3c08dbea1969b40c21
Chandan Ojha <chandanojha776@gmail.com>
2023-11-17 20:03:15 +0600

    `navbar-design-done`

---
+++ nuxt-blog/layouts/default.vue
@@ -0,0 +1,28 @@
+<script setup></script>
+
+<template>
+  <div class="bg-gray-100 min-h-screen text-gray-900">
+    <nav class="bg-white shadow text-lg px-6 py-y">
+      <div class="container mx-auto flex items-center justify-between px-6">
+        <div>
+          <NuxtLink to="/">Logo</NuxtLink>
+        </div>
+        <div>
+          <ul class="flex space-x-12">
+            <li>
+              <NuxtLink to="/">Home</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/about">About</NuxtLink>
+            </li>
+            <li>
+              <NuxtLink to="/contact">Contact</NuxtLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<style scoped></style>
